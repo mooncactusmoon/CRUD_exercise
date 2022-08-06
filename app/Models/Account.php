b2f0d938c351889df160498c2dfdc2d2264e0254
moon <mooncactusmoon@yahoo.com>
2022-08-06 21:18:52 +0800

    建立account的RMC

---
+++ app/Models/Account.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'account',
+        'name',
+        'gender',
+        'birthday',
+        'email',
+        'remark',
+    ];
+
+    protected $casts =[
+        'birthday' => 'datetime:Y-m-d',
+    ];
+}
